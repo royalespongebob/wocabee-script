e9407869fb8bbc7bdaef0a2432a99a3bc8d92ce6
Royale Spongebob <royalespongebob@gmail.com>
2022-04-25 09:30:21 +0200

    created script

---
+++ script.js
@@ -0,0 +1,20 @@
+setInterval(() => {
+    let original = document.querySelector("#q_word").innerText;
+    let elementsList = [...document.querySelectorAll(".locWord")];
+    let inputBox = document.querySelector("#translateWordAnswer");
+    let submitBtn = document.querySelector("#translateWordSubmitBtn");
+        
+    let result = elementsList.find(element => {
+        if (element.getAttribute("word") == original) {
+            return element;
+        }
+    });
+
+    
+    submitBtn.disabled = false;
+    inputBox.value = result.getAttribute("translation");
+    setTimeout(() => {
+        submitBtn.click();
+    }, 200);
+    console.clear();
+}, 3000);
