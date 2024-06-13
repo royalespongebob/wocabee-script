026bc45bbc38c1ebe5bca5237efa70825a82713c
spungi <royalespongebob@gmail.com>
2024-06-13 10:23:37 +0200

    Update file

---
+++ script2.js
@@ -0,0 +1,19 @@
+setInterval(() => {
+    let original = document.querySelector("#q_word").innerText;
+    let elementsList = [...document.querySelectorAll(".locWord")];
+    let inputBox = document.querySelector("#translateWordAnswer");
+    let submitBtn = document.querySelector("#translateWordSubmitBtn");
+
+    let result = elementsList.find(element => {
+        if (element.getAttribute("word") == original) {
+            return element;
+        }
+    });
+
+    submitBtn.disabled = false;
+    inputBox.value = result.getAttribute("translation");
+    setTimeout(() => {
+        submitBtn.click();
+    }, 0); // make sure this value is smaller than the one under
+    console.clear();
+}, 8); // change value in ms to call function in a certain interval
